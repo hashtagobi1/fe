c5e4207c8b4faffa47fe0b698a727b04bee55aee
hashtagobi1 <anokwuruobi@gmail.com>
2023-12-28 06:01:26 +0000

    created function to select language

--- src/components/Footer/FooterLanguage.tsx
+++ src/components/Footer/FooterLanguage.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useRef, useState } from "react";
 import { useQuery } from "react-query";
 // Interfaces and types
 import type {
@@ -21,12 +22,16 @@ import {
   getEmojiFlag,
 } from "countries-list";
 import RightArrow from "@/SVGs/RightArrow";
+import XMark from "@/SVGs/XMark";
 
 type Props = {};
 
 const FooterLanguage = (props: Props) => {
+  const ref = useRef(null);
   const [open, setOpen] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(
+    getCountryData("GB").name
+  );
   const [selectedLanguage, setSelectedLanguage] = useState(languages.en.name);
   const [selectedCurrency, setSelectedCurrency] = useState(
     getCountryData("GB").currency[0]
@@ -62,31 +67,49 @@ const FooterLanguage = (props: Props) => {
       },
     }),
   };
-  console.log(getCountryData("GB"));
-  console.log(selectedLanguage);
-
-  const renderModal = () => {
-    return (
-      <div
-        className={` fixed top-0 left-0 w-full h-full 
-        bg-red-800 duration-500 
-        transition-all
-        flex items-center justify-center
-        z-50 overflow-hidden ${open ? "block" : "hidden"}`}
-      >
-        <h1 onClick={() => setOpen(false)} className="text-red-600 text-5xl">
-          Hi
-        </h1>
-      </div>
-    );
+  const slideVariants = {
+    hidden: { x: "100%" },
+    visible: { x: 0, transition: { duration: 0.1, type: "tween" } },
+    exit: {
+      x: "100%",
+      opacity: 0,
+      transition: { duration: 0.5, type: "tween" },
+    }, // Exit animation
   };
 
-  console.log({ open });
+  const backdropStyle = open
+    ? "fixed inset-0 bg-black opacity-50 z-50"
+    : "hidden";
+
+  const handleChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log({ e });
+    setOpen(false);
+  };
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const name = e.target.value;
+    // console.log(name);
+    e.preventDefault();
+    setSelectedCountry(name);
+    const code = e.target.value ? getCountryCode(name) : "";
+    // console.log(code);
+    const language = name
+      ? languages[getCountryData(getCountryCode(name)).languages[0]].name
+      : "";
+    const currency = name
+      ? getCountryData(getCountryCode(name)).currency[0]
+      : "";
+    // console.log({ language, currency });
+
+    setSelectedLanguage(language);
+    setSelectedCurrency(currency);
+    // console.log({ Selsect: e.target.value });
+  };
   return (
-    <>
+    <div>
       <button
         onClick={() => {
-          setOpen(!open), console.log("hi");
+          setOpen(!open);
         }}
         className="mb-8 rounded-full px-6 py-4 font-semibold text-sm flex border border-black"
       >
@@ -97,19 +120,74 @@ const FooterLanguage = (props: Props) => {
           </span>
         </p>
       </button>
-      {open && <div>{renderModal()}</div>}
-    </>
+      {open && (
+        <AnimatePresence>
+          <div
+            className={`${backdropStyle} text-black `}
+            // onClick={() => setOpen(false)}
+          >
+            <motion.div
+              initial={"hidden"}
+              animate={open ? "visible" : "hidden"}
+              exit={"exit"}
+              variants={slideVariants}
+              className={`  flex-col fixed opacity-100 top-0 right-0 w-4/6 h-full 
+            bg-white duration-500 
+            transition-all
+            flex p-8
+            z-50 overflow-hidden ${open ? "block" : "hidden"}`}
+              //   onClick={() => setOpen(false)}
+            >
+              <div className="flex w-full justify-between">
+                <h1 className="text-2xl font-bold">Select your location</h1>
+                <div className="cursor-pointer" onClick={() => setOpen(false)}>
+                  <XMark />
+                </div>
+              </div>
+
+              <form className="mt-6">
+                <div>
+                  <select
+                    className="bg-gray-100 w-full rounded-full p-4 "
+                    onChange={(e) => handleSelectChange(e)}
+                    value={selectedCountry}
+                  >
+                    {getCountryDataList().map((country) => {
+                      const cc = getCountryCode(country.name);
+                      const emoji = cc ? getEmojiFlag(cc) : "";
+                      return (
+                        <option className="" key={country.name}>
+                          {country.name}
+                          {/* {emoji} */}
+                          <RightArrow />
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="flex p-4 mt-6 font-light">
+                  <button
+                    className="bg-gradient-to-r py-2 px-4 rounded-full from-brandPurple to-brandBlue text-white"
+                    onClick={(e) => handleChange(e)}
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                  <button
+                    className="underline ml-8 underline-offset-8"
+                    onClick={() => setOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </motion.div>
+          </div>
+        </AnimatePresence>
+      )}
+    </div>
   );
 };
 
 export default FooterLanguage;
-
-{
-  /* <motion.section
-  variants={opacity}
-  animate={isInView ? "open" : "closed"}
-  ref={ref}
-  className="p-5 mt-20"
-  data-scroll
-></motion.section>; */
-}
